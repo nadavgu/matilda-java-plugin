5590901f328e0bb7abd0f31d1626af8a92f9811d
nadavgu <nadavgu@gmail.com>
2024-09-14 19:24:38 +0300

    Updated versions

--- agent-plugin/plugin/build.gradle.kts
+++ agent-plugin/plugin/build.gradle.kts
@@ -19,8 +19,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.matilda:commands-generator-api:0.3.0")
-    kapt("org.matilda:commands-generator:0.3.0")
+    implementation("org.matilda:commands-generator-api:0.4.0")
+    kapt("org.matilda:commands-generator:0.4.0")
     kapt("com.google.dagger:dagger-compiler:2.47")
     implementation(kotlin("stdlib-jdk8"))
 }
